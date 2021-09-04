daf148e454db0e6e2fb1a5e03eca5d06fe8aefe3
David Calabrese <david.thomas.calabrese@gmail.com>
2021-09-04 16:57:45 -0500

    makes navbar responsive

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const navItems = document.querySelector(".nav-items");
 const navBarToggle = document.querySelector("#navbar-toggle");
 
 navBarToggle.addEventListener("click", () => {
+    console.log("toggled");
     navItems.classList.toggle("active");
 });
+
+
