24590ceddbcea9f2e1d09c3d66e940e39bb8aed4
David Calabrese <david.thomas.calabrese@gmail.com>
2021-10-02 10:38:25 -0500

    Adds dark mode toggle
    Does not save user preferences yet

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ navBarToggle.addEventListener("click", () => {
 });
 
 
+// darkmode
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.documentElement.setAttribute("data-theme", "light");
+
+    // Get our button switcher
+    let themeSwitcher = document.getElementById("theme-switcher");
+
+    // When our button gets clicked
+    themeSwitcher.onclick = function() {
+      // Get the current selected theme, on the first run
+      // it should be `light`
+      let currentTheme = document.documentElement.getAttribute("data-theme");
+
+      // Switch between `dark` and `light`
+      let switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+      // Set our currenet theme to the new one
+      document.documentElement.setAttribute("data-theme", switchToTheme);
+    }
+  });
