971faeceae90f53c34ea3257d9db9596c62b7d44
David Calabrese <david.thomas.calabrese@gmail.com>
2022-01-03 11:22:27 -0600

    new navbar

--- js/script.js
+++ js/script.js
@@ -1,44 +1,43 @@
-// grab references
 
-let btn = document.querySelector("#theme-switcher");
 
 // navbar functionality
-window.setTimeout(() => {
-  const navItems = document.querySelector(".nav-items");
-  const navBarToggle = document.querySelector("#navbar-toggle");
+// window.setTimeout(() => {
+//   const navItems = document.querySelector(".nav-items");
+//   const navBarToggle = document.querySelector("#navbar-toggle");
 
-  navBarToggle.addEventListener("click", () => {
-    navItems.classList.toggle("active");
-  });
+//   navBarToggle.addEventListener("click", () => {
+//     navItems.classList.toggle("active");
+//   });
 
-  // if navbar is toggled for small screen and screen resizes past 
-  // breakpoint (622px), toggle the navbar again
-  window.addEventListener('resize', () => {
-    const width = getWidth();
-    if (navItems.classList.contains("active")) {
-        if (width > 622) {
-          navItems.classList.toggle("active");
-        }
-    }
-  })
-}, 500)
+//   // if navbar is toggled for small screen and screen resizes past 
+//   // breakpoint (622px), toggle the navbar again
+//   window.addEventListener('resize', () => {
+//     const width = getWidth();
+//     if (navItems.classList.contains("active")) {
+//         if (width > 622) {
+//           navItems.classList.toggle("active");
+//         }
+//     }
+//   })
+// }, 500)
 
 // darkmode logic
-const currentTheme = localStorage.getItem("theme");
+// let btn = document.querySelector("#theme-switcher");
+// const currentTheme = localStorage.getItem("theme");
 
-document.addEventListener('DOMContentLoaded', function(event) {
-  if (currentTheme == "dark") {
-      document.body.classList.toggle("dark");
-    } else if (currentTheme == "light") {
-      document.body.classList.toggle("light");
-    }
+// document.addEventListener('DOMContentLoaded', function(event) {
+//   if (currentTheme == "dark") {
+//       document.body.classList.toggle("dark");
+//     } else if (currentTheme == "light") {
+//       document.body.classList.toggle("light");
+//     }
 
-    btn.addEventListener("click", function() { 
-      document.body.classList.toggle("dark");
-      var theme = document.body.classList.contains("dark") ? "dark" : "light";
-      localStorage.setItem("theme", theme);
-  });
-})
+//     btn.addEventListener("click", function() { 
+//       document.body.classList.toggle("dark");
+//       var theme = document.body.classList.contains("dark") ? "dark" : "light";
+//       localStorage.setItem("theme", theme);
+//   });
+// })
 
 
 
