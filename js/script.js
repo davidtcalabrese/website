a745bae0ed6cb182c5b49d06e6a55d18fe65edf5
David Calabrese <David.thomas.Calabrese@gmail.com>
2024-11-22 16:06:06 -0600

    Nav redesign

--- js/script.js
+++ js/script.js
@@ -1,53 +1,45 @@
-
-
-// navbar functionality
-// window.setTimeout(() => {
-//   const navItems = document.querySelector(".nav-items");
-//   const navBarToggle = document.querySelector("#navbar-toggle");
-
-//   navBarToggle.addEventListener("click", () => {
-//     navItems.classList.toggle("active");
-//   });
-
-//   // if navbar is toggled for small screen and screen resizes past 
-//   // breakpoint (622px), toggle the navbar again
-//   window.addEventListener('resize', () => {
-//     const width = getWidth();
-//     if (navItems.classList.contains("active")) {
-//         if (width > 622) {
-//           navItems.classList.toggle("active");
-//         }
-//     }
-//   })
-// }, 500)
-
 // darkmode logic
 let btn = document.querySelector("#theme-switcher");
 
 const currentTheme = localStorage.getItem("theme");
 
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   if (currentTheme == "dark") {
-      document.body.classList.toggle("dark");
-    } else if (currentTheme == "light") {
-      document.body.classList.toggle("light");
-    }
+    document.body.classList.toggle("dark");
+  } else if (currentTheme == "light") {
+    document.body.classList.toggle("light");
+  }
 
-    btn.addEventListener("click", function() { 
-      document.body.classList.toggle("dark");
-      var theme = document.body.classList.contains("dark") ? "dark" : "light";
-      localStorage.setItem("theme", theme);
+  btn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+    var theme = document.body.classList.contains("dark") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
   });
-})
+});
 
 
+function setLinkColor() {
+  const currentPath = window.location.pathname.split("/").pop();
 
-function getWidth() {
-  return Math.max(
-    document.body.scrollWidth,
-    document.documentElement.scrollWidth,
-    document.body.offsetWidth,
-    document.documentElement.offsetWidth,
-    document.documentElement.clientWidth
-  );
+  const navItems = document.querySelectorAll(".nav-item");
+
+  navItems.forEach((item) => {
+    const link = item.querySelector("a");
+
+    if (link && link.getAttribute("href") === currentPath) {
+      if (currentTheme == "dark") {
+        link.style.color = "#fac901";
+      } else {
+        link.style.color = "#957b15";
+      }
+    }
+  });
 }
+setLinkColor();
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    console.log("Setting link colors after delay...");
+    setLinkColor();
+  }, 100); 
+});
