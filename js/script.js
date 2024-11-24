ea5978d1c0abff7605afcf6fc901e810819dae3b
David Calabrese <David.thomas.Calabrese@gmail.com>
2024-11-23 20:44:23 -0600

    Fix js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let btn = document.querySelector("#theme-switcher");
 const currentTheme = localStorage.getItem("theme");
 
 document.addEventListener("DOMContentLoaded", function (event) {
+
   if (currentTheme == "dark") {
     document.body.classList.toggle("dark");
   } else if (currentTheme == "light") {
@@ -14,11 +15,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.body.classList.toggle("dark");
     var theme = document.body.classList.contains("dark") ? "dark" : "light";
     localStorage.setItem("theme", theme);
+    setNavColor(localStorage.getItem("theme"));
+
+    setTimeout(() => setLinkColor(localStorage.getItem("theme")), 100);
   });
 });
 
 
-function setLinkColor() {
+function setLinkColor(currentTheme) {
   const currentPath = window.location.pathname.split("/").pop();
 
   const navItems = document.querySelectorAll(".nav-item");
@@ -27,19 +31,33 @@ function setLinkColor() {
     const link = item.querySelector("a");
 
     if (link && link.getAttribute("href") === currentPath) {
-      if (currentTheme == "dark") {
+      if (currentTheme == "dark") {        
         link.style.color = "#fac901";
       } else {
-        link.style.color = "#957b15";
+        link.style.color = "#000";
       }
     }
   });
 }
-setLinkColor();
+
+function setNavColor(currentTheme) {
+  setTimeout(() => {
+    const nav = document.querySelector("#nav");
+
+    if (currentTheme == "dark") {
+      nav.classList.remove("navbar-light");
+      nav.classList.add("navbar-dark");
+    } else if (currentTheme == "light") {
+      nav.classList.remove("navbar-dark");
+      nav.classList.add("navbar-light");
+    }
+  
+  }, 100);
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    console.log("Setting link colors after delay...");
-    setLinkColor();
+    setLinkColor(localStorage.getItem("theme"));
+    setNavColor(localStorage.getItem("theme"));
   }, 100); 
 });
